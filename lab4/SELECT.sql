ec7522f8eb66717b80d961baf804e0dec7a25ef4
YuliaSheb <79199794+YuliaSheb@users.noreply.github.com>
2022-11-28 14:18:54 +0300

    Update SELECT.sql

--- lab4/SELECT.sql
+++ lab4/SELECT.sql
@@ -1,13 +1,19 @@
+SELECT Id, Name 
+FROM Ingredient  
+WHERE (Name LIKE 'С%') OR (Name ILIKE 'М%');  
+
 SELECT  Name,
         (SELECT TypeName FROM TypeDessert 
         WHERE Typedessert.Id = Dessert.TypeDessertId) AS Type
 FROM Dessert;
+
 SELECT  Name,Surname,Patronomic,
         (SELECT Login FROM Account 
         WHERE Account.Id = Confections.AccountId) AS Log,
 		(SELECT TypeName FROM TypeDessert 
         WHERE TypeDessert.Id = Confections.TypeDessertId) AS Des
 FROM Confections;
+
 SELECT p.Id, p.Name
 FROM Dessert p
 WHERE p.Confectionsid IN
